ba841839680171b168824f4932dfc03b81a7af95
Zainab Olurode <bintjash@gmail.com>
2020-03-17 22:18:00 +0100

    Styling and Scripts for login and signup tabs

--- Script/main.js
+++ Script/main.js
@@ -1,16 +1,36 @@
-/*Start of script for login and signin pages*/
- $(document).ready(function(){
-         $(".register").click(function(){
-            $(".other").show();
-            $(".content").hide();
-            $(".register").addClass('active');
-            $(".login").removeClass('active');
-         });
-         $(".login").click(function(){
-            $(".content").show();
-            $(".other").hide();
-            $(".login").addClass('active');
-            $(".register").removeClass('active');
-         });
+function setupTabs () {
+   document.querySelectorAll(".tabs__button").forEach(button => {
+     button.addEventListener("click", () => {
+        const sideBar = button.parentElement;
+        const tabsContainer = sideBar.parentElement;
+        const tabNumber = button.dataset.forTab;
+        const tabToActivate = tabsContainer.querySelector(`.tabs__content[data-tab = "${tabNumber}"]`);
+
+
+        sideBar.querySelectorAll(".tabs__button").forEach(button => {
+            button.classList.remove(".tabs__button--active");
+        });
+        tabsContainer.querySelectorAll(".tabs__content").forEach(tab => {
+            tab.classList.remove(".tabs__content--active");
+        });
+        button.classList.add(".tabs__button--active");
+        tabToActivate.classList.add(".tabs__content--active");
+
+        console.log(sideBar);
+        console.log(tabsContainer);
+        console.log(tabNumber);
+        console.log(tabToActivate);
+
+
+
+     });
+   });
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+   setupTabs();
+   document.querySelectorAll(".tabs").forEach(tabsContainer => {
+      tabsContainer.querySelector(".tabs__sidebar .tabs__button").click();
+   });
 });
 /*End of script for login and signin pages*/
